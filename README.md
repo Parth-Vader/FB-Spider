d41671465a9ddd05a946871a020c725485341599
Parth Verma <vermaparth97@gmail.com>
2016-09-23 20:53:15 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,8 @@
+# FB-Crawler
+A program which accepts the id of a Facebook page.
+It makes a table in .html format of the latest 5 posts and the respective latest 5 comments per post.
+
+The number of posts and comments can be changed by editing the graph.py file.
+
+Please replace the access token by your app's access token.
+s
--- graph.py
+++ graph.py
@@ -1,11 +1,13 @@
 from facepy import GraphAPI
 import json
 from json2html import *
-#import webbrowser
+import webbrowser
 graph= GraphAPI('EAAL76SmH3vMBAC9ADmDLE0E8ZBCmVZAcwSP8IB5CtlVDhLLT9DwbufvjZAjMjSfTecN4RTRZBIHM4bKQmSF7y7kFbSYcIdYXJeqtP2dbyZB7jgBS8XVpZCTTveYoGJFEvkLawfX1nb2cnjavWHmFlrilhDYhtspfgZD')
 
 print("Please enter the page-id  " )
 PageId=raw_input()
+
+graph= GraphAPI('EAAL76SmH3vMBAC9ADmDLE0E8ZBCmVZAcwSP8IB5CtlVDhLLT9DwbufvjZAjMjSfTecN4RTRZBIHM4bKQmSF7y7kFbSYcIdYXJeqtP2dbyZB7jgBS8XVpZCTTveYoGJFEvkLawfX1nb2cnjavWHmFlrilhDYhtspfgZD')
 
 variable = graph.get(str(PageId)+'/posts?fields=comments.limit(5){message},message&since=today&limit=5')
 
@@ -22,6 +24,6 @@ f = open('Table.html','w')
 f.write(htmlfile)
 f.close()
 
-#webbrowser.open("home/Desktop/Table.html")
+webbrowser.open("Table.html")
 #webbrowser.open('www.google.co.in')
 #print(variable)
