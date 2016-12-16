3a2aa747632807fc435a98907bcf429a97d2eff9
sarthak5 <sarthakmadaan5121995@gmail.com>
2016-12-16 18:31:12 +0530

    modified table, added created time and name of commenter, removed unwanted elements

--- graph.py
+++ graph.py
@@ -16,13 +16,23 @@ for index,item in enumerate(search_res['data']):
 pno=int(raw_input("Please enter the page no. : "))
 pid=search_res['data'][pno-1]['id']        
       
-variable = graph.get(pid+'/posts?fields=comments.limit(5){message},message&limit=5')
+variable = graph.get(pid+'/posts?fields=comments.limit(5),message&limit=5')
         
 with open('data.json', 'wb') as outfile:
     json.dump(variable, outfile)
 
+with open('data.json') as js:
+	jsvar=json.load(js)
+	del jsvar['paging']
+	for i in range(0,5):
+		del jsvar['data'][i]['id']
+		del jsvar['data'][i]['comments']['paging']
+		for j in range(0,len(jsvar['data'][i]['comments']['data'])):
+			del jsvar['data'][i]['comments']['data'][j]['id']
+			del jsvar['data'][i]['comments']['data'][j]['from']['id']
+
 #infoFromJson = json.loads(variable)
-table = json2html.convert(json = variable)
+table = json2html.convert(json = jsvar)
 htmlfile=table.encode('utf-8')
 #print(htmlfile)
 f = open('Table.html','w')
