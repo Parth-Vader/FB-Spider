ab3396303db3f13b73ab0de268a32a922b907d0a
sarthak5 <sarthakmadaan5121995@gmail.com>
2016-12-17 19:38:24 +0530

    removed jsvar, made some changes

--- graph.py
+++ graph.py
@@ -10,29 +10,31 @@ PageName=raw_input()
 search_res=graph.get('search?q='+PageName+'&type=page&limit=5')
 
 for index,item in enumerate(search_res['data']):
-    #The 'data' key of 'search_res' dictionary is a list of dictionaries of 5 pages
-    print index+1,item['name']
-    
+	#The 'data' key of 'search_res' dictionary is a list of dictionaries of 5 pages
+	print index+1,item['name']
+
 pno=int(raw_input("Please enter the page no. : "))
 pid=search_res['data'][pno-1]['id']        
-      
-variable = graph.get(pid+'/posts?fields=comments.limit(5),link,message&limit=5')
-        
-with open('data.json', 'wb') as outfile:
-    json.dump(variable, outfile)
 
-with open('data.json') as js:
-	jsvar=json.load(js)
-	del jsvar['paging']
-	for i in range(0,5):
-		del jsvar['data'][i]['id']
-		del jsvar['data'][i]['comments']['paging']
-		for j in range(0,len(jsvar['data'][i]['comments']['data'])):
-			del jsvar['data'][i]['comments']['data'][j]['id']
-			del jsvar['data'][i]['comments']['data'][j]['from']['id']
+variable = graph.get(pid+'/posts?fields=comments.limit(5),link,message&limit=5')
+
+del variable['paging']
+for i in range(0,5):
+	del variable['data'][i]['id']
+	try:
+		del variable['data'][i]['comments']['paging']
+		for j in range(0,len(variable['data'][i]['comments']['data'])):
+			del variable['data'][i]['comments']['data'][j]['id']
+			del variable['data'][i]['comments']['data'][j]['from']['id']
+	except:pass
+
+
+with open('data.json', 'wb') as outfile:
+	json.dump(variable, outfile)
+
 
 #infoFromJson = json.loads(variable)
-table = json2html.convert(json = jsvar)
+table = json2html.convert(json = variable)
 htmlfile=table.encode('utf-8')
 #print(htmlfile)
 f = open('Table.html','w')
