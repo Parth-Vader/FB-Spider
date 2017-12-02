16da87925cc07eeb8812e904cb1367c3419a1056
Dibya Ranjan Jena <dibyajena917@gmail.com>
2017-12-03 00:35:13 +0530

    Created setup.py
    Run pip install --editable .   or  python setup.py install 
    Note:make sure u are doing this in virtualenv of python3 else use pip3 and python3 for (python 3)

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name ='FB-Spider',
+    version ='0.1',
+    description = 'accepts the id of a Facebook page and transforms into a table of the latest 5 posts and the respective latest 5 comments per post. The table will be in .html format',
+    author = 'Parth Verma',
+    author_email = 'vermaparth97@gmail.com',
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires=[
+        'facepy',
+        'json2html',
+    ]
+)
