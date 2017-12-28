e4da3a2a1e61aa1ece7b84a3a738f754bca3a47c
Dibya Ranjan Jena <dibyajena917@gmail.com>
2017-12-29 00:43:06 +0530

    Update setup.py
    update setup.py to support cli feature added in graph.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name ='FB-Spider',
-    version ='1.0',
+    version ='2.0',
     description = 'accepts the id of a Facebook page and transforms into a table of the latest 5 posts and the respective latest 5 comments per post. The table will be in .html format',
     author = 'Parth Verma',
     author_email = 'vermaparth97@gmail.com',
@@ -16,7 +16,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        fbs=graph:access_token_handler
-        fbspider=graph:page_handler
+        fbspider=graph:cli
     ''',
 )
